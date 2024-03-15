406ada92b17ae2b1c950f8f6556e92978ec475ed
Lux <Lux-63@yandex.ru>
2024-03-15 18:41:03 +0500

    1.1.0

--- script_gallows.js
+++ script_gallows.js
@@ -1,77 +1,80 @@
-//document.addEventListener("keydown", handleKey);
-
 let nikName = prompt('Добро пожаловать в игру "Виселица" как вас зовут?');
 const gameInfo = document.querySelector(".information");
 const hiddenWord = document.querySelector(".progress");
-const privet = document.querySelector(".frame"); 
-const meaning = document.getElementById("input");
+const privet = document.querySelector(".frame");
+const meaning = document.querySelector(".box-2");
+const reset = document.querySelector(".reset");
+const stopGames = document.querySelector(".end");
+let a = "";
+
+privet.innerHTML = `<p>Привет, <b>${nikName}.</b> <br>Начнем игру</p>`;
+console.log(`Привет, ${nikName}`);
 
 
 
-privet.innerHTML =`<p>Привет, <b>${nikName}.</b> <br>Начнем игру</p>` 
-console.log(`Привет, ${nikName}`)
+let words = ["программа", "макака", "прекрасный", "оладушек", "чебурек"];
+let word = words[Math.floor(Math.random() * words.length)];
+let attempts = 7;
+let answer = [];
+for (let i = 0; word.length > i; i++) {
+  answer[i] = "_";
+}
+let remainingLetters = word.length;
+console.log(word);
+// Показываем состояние игры
+hiddenWord.innerHTML = answer.join(" ");
 
-document.getElementById("check").addEventListener("click", () => {
-    console.log(meaning.value);
-    meaning.value = "";
-    meaning.focus();
-})
-    
+gameInfo.innerHTML = 'Угадайте букву или нажмите "Отмена" для выхода из игры.';
 
-//function handleKey(event) {
+function stop() {
+    a = "pause";
+    console.log(a);
+}
+function restart() {
+    a = "restart";
+    console.log(a);
+}
+function checkAd() {
+  a = meaning.value.toLowerCase();
+  meaning.value = "";
+  meaning.focus();
+  console.log(a);
+  gameInfo.innerHTML = 'Угадайте букву или нажмите "Отмена" для выхода из игры.';
+}
 
+  while (remainingLetters > 0 && attempts > 0) {
+    console.log("условие выполняется",)
 
-/**
- * массив слов
- */
-let words = [
-    "программа",
-    "макака",
-    "прекрасный",
-    "оладушек",
-    "чебурек"
-    ];
-
-    let word = words[Math.floor(Math.random() * words.length)];
-    let attempts = 7;
-
-    let answer = [];
-    for (let i = 0; word.length > i; i++) {
-        answer[i] = "_";
-    }
-    let remainingLetters = word.length;
-    console.log(word);
-    // Игровой цикл
-    while (remainingLetters > 0 && attempts > 0) {
-    // Показываем состояние игры
-    hiddenWord.innerHTML = answer.join(" ");
-    // Запрашиваем вариант ответа
-
-    let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.".toLowerCase());
-    if (guess === null) {
-        break;
-    } else if (guess.length !== 1) {
-        gameInfo.innerHTML= "Пожалуйста, введите только одну букву.";
+    if (a == "pause") {
+        gameInfo.innerHTML = "Конец игры";
+        console.log("Конец игры")
+    } if (a === "restart"){
+        word = words[Math.floor(Math.random() * words.length)];
+        console.log(word)
+    } if (aasdGf.length !== 1) {
+      gameInfo.innerHTML = "Пожалуйста, введите только одну букву.";
     } else {
-        for (let j = 0; j < word.length; j++) {
-            if (answer[j] === guess) {
-                gameInfo.innerHTML=`${nikName} вы уже угадали эту букву!`
-                break;
-            } else if (word[j] === guess) {
-                answer[j] = guess;
-                remainingLetters--;
-            }
+      for (let j = 0; j < word.length; j++) {
+        if (answer[j] === a) {
+          gameInfo.innerHTML = `${nikName} вы уже угадали эту букву!`;
+          break;
+        } else if (word[j] === a) {
+          answer[j] = a;
+          remainingLetters--;
         }
-        if (word.includes(guess) == false && attempts >= 1) {
-            gameInfo.innerHTML=`${nikName} такой буквы нету. У вас осталось попыток: "${attempts - 1}"`
-            attempts--;
-            console.log(attempts)
-        }
+      }
+      if (word.includes(a) == false && attempts >= 1) {
+        gameInfo.innerHTML = `${nikName} такой буквы нету. У вас осталось попыток: "$`
+        attempts - 1
+        };
+        attempts--;
+        console.log(attempts);
+      }
     }
-}
-if (attempts == 0) {
-    alert('вы проиграли! Было загадано слово "' + word +'"');
-} else {
+  if (attempts == 0) {
+    alert('вы проиграли! Было загадано слово "' + word + '"');
+  } else {
     alert(answer.join(" "));
-    alert('Отлично! Было загадано слово "' + word +'"');
-}
+    alert('Отлично! Было загадано слово "' + word + '"');
+  }
+
