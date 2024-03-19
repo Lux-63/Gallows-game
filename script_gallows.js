f1be1eec31c9b4f646a71eb608683c737c265b6f
Lux-63 <Lux-63@yandex.ru>
2024-03-19 20:04:50 +0500

    canvas

--- script_gallows.js
+++ script_gallows.js
@@ -1,6 +1,8 @@
-const nikName = prompt('Добро пожаловать в игру "Виселица" как вас зовут?');
-
+const nikName = prompt('Добро пожаловать в игру "Виселица" как вас зовут?')
+//массив слов
 let wordsArray = ["программа", "макака", "прекрасный", "оладушек", "чебурек"];
+//мыссив функций canvas
+let lifeField = document.querySelector(".canvas").getContext("2d");
 
 const gameInfoElem = document.querySelector(".information");
 const hiddenWordElem = document.querySelector(".progress");
@@ -18,6 +20,72 @@ topWindowElem.innerHTML = `<p>Привет, <b>${nikName}.</b> <br>Начнем 
 generationWord();
 console.log(`Привет, ${nikName}`, randomWord);
 
+
+/**
+ * функции Canvas
+ */
+function oneLife () {
+    lifeField.beginPath();
+    lifeField.moveTo(0, 180);
+    lifeField.lineTo(160, 180);
+    lifeField.moveTo(30, 180);
+    lifeField.lineTo(30, 20);
+    lifeField.moveTo(10, 20);
+    lifeField.lineTo(100, 20);
+    lifeField.moveTo(90, 20);
+    lifeField.lineTo(90, 75);
+    lifeField.lineWidth = 2;
+    lifeField.stroke();
+}  lifeField.clearRect(0, 0, canvas.width, canvas.height);
+function  twoLife() {
+    lifeField.beginPath();
+    lifeField.clearRect(70, 45, 40, 40)
+    lifeField.arc(90, 60, 15, 300, 360, false);
+    lifeField.lineWidth = 2;
+    lifeField.stroke();
+}
+function threeLife() {
+    lifeField.beginPath();
+    lifeField.moveTo(90, 75);
+    lifeField.lineTo(90, 130);
+    lifeField.lineWidth = 2; 
+    lifeField.stroke();
+}
+function fourLife() {
+    lifeField.beginPath();
+    lifeField.moveTo(90, 85);
+    lifeField.lineTo(70, 110);
+    lifeField.lineWidth = 2;
+    lifeField.stroke();
+}
+function fiveLife() {
+    lifeField.beginPath();
+    lifeField.moveTo(90, 85);
+    lifeField.lineTo(110, 110);
+    lifeField.lineWidth = 2;
+    lifeField.stroke();
+}
+function sixLife() {
+    lifeField.beginPath();
+    lifeField.moveTo(90, 130);
+    lifeField.lineTo(70, 160);
+    lifeField.lineWidth = 2;
+    lifeField.stroke();
+}
+function sevenLife() {
+    lifeField.beginPath();
+    lifeField.moveTo(90, 130);
+    lifeField.lineTo(110, 160);
+    lifeField.lineWidth = 2; 
+    lifeField.stroke();
+}
+//массив количества жизней
+let chanceLife = [sevenLife, sixLife, fiveLife, fourLife, threeLife, twoLife, oneLife];
+
+
+
+
+
 // выбор случайного слова
 /**
  * генерация слова, замена букв на символы
@@ -29,6 +97,7 @@ function generationWord() {
   for (let i = 0; randomWord.length > i; i++) {
     answer[i] = "_";
   }
+  lifeField.clearRect(0, 0, canvas.width, canvas.height);
   hiddenWordElem.innerHTML = answer.join(" ");
   remainingLetters = randomWord.length;
   console.log(randomWord, remainingLetters);
@@ -47,6 +116,7 @@ function checkLetter() {
     console.log("введено меньше или больше одной буквы");
   } else if (attempts == 0) {
     gameInfoElem.innerHTML = `${nikName} Вы проиграли! Было загадано слово "${randomWord}"`;
+    chanceLife[attempts]();
     console.log("Проигрыш");
   } else {
     gameInfoElem.innerHTML = 'Угадайте букву или нажмите "Начать заново что бы сменить слово.';
@@ -59,8 +129,9 @@ function checkLetter() {
  * @param {Text} meaning 
  */
 function gameProcess(meaning) {
-  if (randomWord.includes(meaning) == false && attempts >= 1) {
+  if (randomWord.includes(meaning) == false && attempts >= 0) {
     gameInfoElem.innerHTML = `такой буквы нету. У вас осталось попыток: "${attempts}"`;
+    chanceLife[attempts]();
     attempts--;
     console.log("минус жизнь", attempts);
   }
@@ -75,7 +146,7 @@ function gameProcess(meaning) {
       gameInfoElem.innerHTML = `Поздравляем! Такая буква есть. Следующая буква?.`
       console.log("Буква найдена", answer);
       if (answer.includes("_") == false) {
-        gameInfoElem.innerHTML = `'Хорошо! ${nikName} Было загадано слово "${randomWord}"`;
+        gameInfoElem.innerHTML = `Хорошо! ${nikName} Было загадано слово "${randomWord}"`;
         console.log("Победа");
     }
   }
